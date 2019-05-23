29fe7aa63be0d63fd68b004896350c5e62da61cf
yanqixuan <1402412256@qq.com>
2019-05-23 22:15:38 +0800

    socket接口

---
+++ Socket/index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import morgan from 'morgan'
+import path from 'path'
+import socketIO from 'socket.io'
+// 服务器端
+const app = express()
+app.set('views',path.resolve(__dirname,'views'))
+app.set('view engine','ejs')
+
+app.use(morgan('dev'))
+
+app.get('/',(request,response)=>{
+    response.render('index')
+})
+
+let server = app.listen(8080,()=>{
+    console.log('66666')
+})
+
+let io = socketIO(server)
+// 监听连接事件
+io.on('connection',(socket)=>{
+    console.log('connected')
+    socket.on('disconnect',()=>{
+        console.log('disconnected')
+    })
+    socket.on('message',(msg)=>{
+        console.log("msg");
+        io.emit('message',msg);
+    })
+})
---
+++ Socket/readme.md
@@ -0,0 +1,3 @@
+# 自建服务器
+1. yarn 安装
+    yarn add socket -S
